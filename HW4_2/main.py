c5815b8cc94f123635b08a3754a7d3ce023f4354
hk101b13 <hk101b13@gmail.com>
2021-06-13 10:47:02 -0700

    107000212

--- HW4_2/main.py
+++ HW4_2/main.py
@@ -8,8 +8,8 @@ sensor.set_framesize(sensor.QQVGA)
 sensor.skip_frames(time = 2000)
 clock = time.clock()
 
-# All lines also have x1(), y1(), x2(), and y2() methods to get their end-points
-# and a line() method to get all the above as one 4 value tuple for draw_line().
+# All lines also have `x1()`, `y1()`, `x2()`, and `y2()` methods to get their end-points
+# and a `line()` method to get all the above as one 4 value tuple for `draw_line()`.
 low_threshold = (0, 50)
 uart = pyb.UART(3,9600,timeout_char=1000)
 uart.init(9600,bits=8,parity = None, stop=1, timeout_char=1000)
@@ -17,15 +17,17 @@ uart.init(9600,bits=8,parity = None, stop=1, timeout_char=1000)
 while(True):
    clock.tick()
    img = sensor.snapshot().binary([THRESHOLD]) if BINARY_VISIBLE else sensor.snapshot()
-   #if enable_lens_corr: img.lens_corr(1.😎 # for 2.8mm lens...
+   #if enable_lens_corr: img.lens_corr(1.8) # for 2.8mm lens...
    line = img.get_regression([(255,255) if BINARY_VISIBLE else THRESHOLD],roi = (20,0,140,70), robust = True)
    if (line): img.draw_line(line.line(),color =127)
 
 
-   print(line.rho())
-   print_arg=(line.rho())
+   #print(line.rho())
+   if(line.rho()<0):
+     dir = "l"
+   elif(line.rho()>0):
+     dir = "r"
+   print_arg=(dir)
+   print(dir)
 
-   uart.write(("%f" % print_arg).encode())
-   #uart.write(("%f" % print_args2).encode())
-   #uart.write(("%f" % print_args3).encode())
-   #uart.write(("%f" % print_args4).encode())
+   uart.write(("%s" % print_arg).encode())
